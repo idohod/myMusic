3c3b1da3d1767245803ae1dc1c7f3ce9edc748d2
idohod <127017712+idohod@users.noreply.github.com>
2024-04-20 23:34:38 +0300

    Initial commit

---
+++ Rhythm.py
@@ -0,0 +1,75 @@
+import random
+
+class Rhythm(object):
+    def __init__(self):
+        self.size = 16   
+        self.rhythmNums = []
+        self.HHArray = []
+        self.bassArray = []
+        self.snareArray = []
+    
+    def randArray(self):
+        for i in range(self.size):
+            if(i == 0 or i == 8):
+                  self.rhythmNums.append(1)
+            elif(i == 4 or i == 12):
+                  self.rhythmNums.append(2)
+            else:
+                  self.rhythmNums.append(random.randint(0,2))
+        return 
+    def checkRandom(self):
+        stackNums = [4,8,12]
+        for i in range(self.size - 2):
+           if(self.rhythmNums[i] == self.rhythmNums[i + 1] and self.rhythmNums[i + 1] == self.rhythmNums[i + 2]):
+              if(i + 2 in stackNums):
+                  self.rhythmNums[i] = 0
+              else:
+                  self.rhythmNums[i + 2] = 0
+           
+           if(self.rhythmNums[self.size - 2] == self.rhythmNums[self.size - 1] and self.rhythmNums[self.size - 2] == self.rhythmNums[0]):
+              self.rhythmNums[self.size - 1] = 0
+        return
+
+    def putHH(self):
+         for i in range(self.size):
+            if(i % 2 == 0):
+                 self.HHArray.append("HH")
+            else:
+                 self.HHArray.append(" ")
+         return
+
+    def setRhythm(self):
+       
+       for i in self.rhythmNums:      
+            if(i == 1):
+               self.bassArray.append("BASS")
+               self.snareArray.append(" ")
+            elif(i == 2):
+                self.snareArray.append("SNARE") 
+                self.bassArray.append(" ")
+            else:
+                self.bassArray.append(" ")
+                self.snareArray.append(" ")
+       return
+
+    def randAndSetRhythm(self):
+        self.randArray()
+        self.checkRandom()
+        self.putHH()
+        self.setRhythm()
+        return
+
+    def temp(self,buffer,arr):
+        for i in range(self.size):
+            buffer += arr[i]
+            buffer += "\t"        
+        buffer += "\n"
+        return buffer
+
+    def rhythmInfo(self):
+        info = ""
+        hh = self.temp(info,self.HHArray)
+        snare = self.temp(info,self.snareArray)
+        bass = self.temp(info,self.bassArray)
+        info = hh+"\n"+snare+bass
+        return info 
---
+++ Transition.py
@@ -0,0 +1,76 @@
+from Rhythm import Rhythm
+import random
+
+class Transition(object):
+    def __init__(self):
+        self.size = 16   
+        self.transitionNums = []
+        self.Tom1Array = []
+        self.Tom2Array = []
+        self.snareArray = []
+        self.floorArray = []
+    
+    def randTransition(self,index):
+        for i in range(self.size):
+           if(i < index):
+                self.transitionNums.append(-1)
+           else:
+                self.transitionNums.append(random.randint(1,4))
+        return
+
+    def setTransition(self,index,Rhythm):
+      for i in range(self.size):
+        if(i < index):
+            self.Tom1Array.append(Rhythm.HHArray[i])
+            self.Tom2Array.append("")
+            self.snareArray.append(Rhythm.snareArray[i])
+            self.floorArray.append(Rhythm.bassArray[i])
+        else:
+            if(self.transitionNums[i] == 1):            
+                self.Tom1Array.append("TOM1")
+                self.Tom2Array.append("")
+                self.floorArray.append("")
+                self.snareArray.append("")
+           
+            elif(self.transitionNums[i] == 2):
+                self.Tom1Array.append("")
+                self.Tom2Array.append("")
+                self.floorArray.append("")
+                self.snareArray.append("SNARE")
+
+            elif(self.transitionNums[i] == 3):
+                self.Tom1Array.append("")
+                self.Tom2Array.append("TOM2")
+                self.floorArray.append("")
+                self.snareArray.append("")
+
+            elif(self.transitionNums[i] == 4):
+                self.Tom1Array.append("")
+                self.Tom2Array.append("")
+                self.floorArray.append("FLOOR")
+                self.snareArray.append("")
+
+            else:
+                self.Tom1Array.append("")
+                self.Tom2Array.append("")
+                self.floorArray.append("") 
+                self.snareArray.append("")
+
+      return
+
+    def temp(self,buffer,arr):
+        for i in range(self.size):
+            buffer += arr[i]
+            buffer += "\t"        
+        buffer += "\n"
+        return buffer
+
+    def TransitionInfo(self):
+        info = ""
+        tom1 = self.temp(info,self.Tom1Array)
+        tom2 = self.temp(info,self.Tom2Array)
+        snare = self.temp(info,self.snareArray)
+        floor = self.temp(info,self.floorArray)
+        info = tom1+tom2+snare+floor
+        return info
+
